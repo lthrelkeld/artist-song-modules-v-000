8103cd5f8c8f4c704fd4d94af523653082313af2
Elizabeth Karst <efkarst@outlook.com>
2018-08-26 20:56:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     @@songs
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#  def self.find_by_name(name)
+#    @@songs.detect{|a| a.name == name}
+#  end
 
   def artist=(artist)
     @artist = artist
